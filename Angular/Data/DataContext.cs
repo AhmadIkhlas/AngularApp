7af848632bc3e812e3a58d86638acb9d01d2a068
bscs351 <rfatima590>
2019-09-07 10:42:12 +0500

    initial create

---
+++ Angular/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Angular.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Angular.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer(@"data source=DESKTOP-JIUQB1F;initial catalog=AngularApp;persist security info=True;integrated security=true");
+        //}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
